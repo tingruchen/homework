c023cdd69790e503dac8d3ec04ff9aa1f3b2e891
Tingru Chen[陳亭如] <tingru.chen@tingruchen-mac13.local>
2023-09-22 16:03:32 +0800

    init

--- index.php
+++ index.php
@@ -1,24 +1,44 @@
 <!DOCTYPE html>
 <html>
-<body>
-<?php
-  session_start();
-  if (!empty($_SESSION["member"])) { ?>
-    <a href="./user?id=<?php echo $_SESSION['member']['user_id']?>">會員頁面</a><br />
-    <a href="./action/logout.php">登出</a>
-    <hr />
-    <h1><?php echo "歡迎~". $_SESSION["member"]["user_firstName"]; ?></h1>
-  <?php } else { ?>
-    <h1>立即登入</h1>
-    <form action="./action/login.php" method="post">
-      <label for="user_name">*帳號:</label><br>
-      <input type="text" id="user_name" name="user_name" placeholder="ex: ooo@104.com.tw"><br>
-      <label for="user_password">*密碼:</label><br>
-      <input type="password" id="user_password" name="user_password"><br><br>
-      <input type="submit" value="登入">
-    </form>
-  <?php } 
-?>
+  <head>
+    <link href="./style/style.css" rel="stylesheet" type="text/css">
+  </head>
+  <body>
+    <?php
+      session_start();
+      if (!empty($_SESSION["member"])) { ?>
+        <a href="./user?id=<?php echo $_SESSION['member']['user_id']?>" class="btn">會員頁面</a>
+        <a href="./action/logout.php" class="btn">登出</a>
+        <hr />
+        <h1><?php echo "歡迎~". $_SESSION["member"]["user_firstName"]; ?></h1>
+      <?php } else { ?>
+        <h1>立即登入</h1>
+        <form action="./action/login.php" method="post">
+          <label for="user_name">* 帳號:</label><br>
+          <input type="text" id="user_name" name="user_name" placeholder="ex: ooo@104.com.tw"><br>
+          <label for="user_password">* 密碼:</label><br>
+          <input type="password" id="user_password" name="user_password"><br><br>
+          <button type="submit" class="submit">登入</button>
+        </form>
+      <?php } 
+    ?>
+    <?php
+      include "./action/connect.php";
 
-</body>
+      $sql = "SELECT * FROM blog LIMIT 12";
+      $stmt = $connect -> prepare($sql);
+      $stmt -> execute();
+      $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+      if (!empty($result)) {
+        foreach ($result as $blog_key => $blog) {?>
+          <div class="blog-card">
+            <h3><?php echo $blog["blog_title"] ?></h3>
+            <p><?php echo $blog["blog_content"] ?></p>
+          </div>
+        <?php }
+      } 
+      unset($connect);
+    ?>
+  </body>
 </html>
--- user/index.php
+++ user/index.php
@@ -1,17 +1,24 @@
-<?php session_start(); ?>
-<?php if (!$_GET || $_GET["id"] != $_SESSION["member"]["user_id"]) { 
-  include "../error/404.php";
-} else { ?>
-  <a href="/">首頁</a><br />
-  <a href="/action/logout.php">登出</a>
-  <hr />
-  <h1>會員中心</h1>
-  <form action="./action/update.php?id=<?php echo $_GET['id']?>" method="post">
-    <label for="user_firstName">*名字:</label><br>
-    <input type="text" id="user_firstName" name="user_firstName" value="<?php echo $_SESSION['member']['user_firstName']?>"><br>
-    <label for="user_lastName">*姓氏:</label><br>
-    <input type="text" id="user_lastName" name="user_lastName" value="<?php echo $_SESSION['member']['user_lastName']?>"><br><br>
-    <input type="submit" value="修改">
-  </form> 
-<?php } ?>
-
+<!DOCTYPE html>
+<html>
+  <head>
+    <link href="../style/style.css" rel="stylesheet" type="text/css">
+  </head>
+  <body>
+  <?php session_start(); ?>
+  <?php if (!$_GET || $_GET["id"] != $_SESSION["member"]["user_id"]) { 
+    include "../error/404.php";
+  } else { ?>
+    <a href="/" class="btn">首頁</a>
+    <a href="/action/logout.php" class="btn">登出</a>
+    <hr />
+    <h1>會員中心</h1>
+    <form action="./action/update.php?id=<?php echo $_GET['id']?>" method="post">
+      <label for="user_firstName">* 名字:</label><br>
+      <input type="text" id="user_firstName" name="user_firstName" value="<?php echo $_SESSION['member']['user_firstName']?>"><br>
+      <label for="user_lastName">* 姓氏:</label><br>
+      <input type="text" id="user_lastName" name="user_lastName" value="<?php echo $_SESSION['member']['user_lastName']?>"><br><br>
+      <button type="submit" class="submit">修改</button>
+    </form> 
+  <?php } ?>
+  </body>
+</html>
